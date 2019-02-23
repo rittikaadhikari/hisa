2889f15f7b523ef1ad9dd517ab8c54d1a4567bd5
rittikaadhikari <rittika.adhikari@gmail.com>
2019-02-23 15:16:48 -0600

    made makefile

---
+++ Makefile
@@ -0,0 +1,17 @@
+PYTHON = python
+
+install: 
+	cat requirements/*.txt > requirements.txt
+	pip install -r requirements.txt
+
+	pip install tensorflow-gpu
+	
+	$(PYTHON) setup.py install 
+	
+	#bash twitter.sh
+
+clean: 
+	$(PYTHON) setup.py clean 
+
+all: 
+	make install clean
---
+++ package.py
@@ -0,0 +1,34 @@
+# Inspired by npm's package.json file
+name              = 'hisa'
+version           = '0.1.0'
+release           = '0.1.0'
+description       = 'A stock market predictor and model builder'
+long_description  = ['README.md']
+keywords          = ['neural', 'network', 'machine', 'deep',
+    'learning', 'tensorflow', 'stock', 'market', 'prediction']
+authors           = [
+    { 'name': 'Rittika Adhikari', 'email': 'rittika.adhikari@gmail.com' },
+		{ 'name': 'Sahil Modi', 'email': 'sm34524@gmail.com'},
+		{ 'name': 'Utkarsh Awasthi', 'email': 'navamawasthi@gmail.com'}
+]
+maintainers       = [
+    { 'name': 'Rittika Adhikari', 'email': 'rittika.adhikari@gmail.com' },
+		{ 'name': 'Sahil Modi', 'email': 'sm34524@gmail.com'},
+		{ 'name': 'Utkarsh Awasthi', 'email': 'navamawasthi@gmail.com'}
+]
+license           = 'MIT'
+modules           = [
+    'hisa',
+    'hisa.config',
+    'hisa.capsule',
+    'hisa.learn',
+    'hisa.learn.models',
+    'hisa.learn.eval',
+    'hisa.learn.sentiment',
+]
+github_username   = 'rittikaadhikari'
+github_repository = 'hisa'
+github_url        = '{baseurl}/{username}/{repository}'.format(
+    baseurl    = 'https://github.com',
+    username   = github_username,
+    repository = github_repository)
