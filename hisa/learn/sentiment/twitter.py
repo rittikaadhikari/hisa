59c5de8541e6c76b0047619aa59cec2f8383d914
rittikaadhikari <rittika.adhikari@gmail.com>
2019-02-23 16:03:15 -0600

    adding utils

---
+++ hisa/learn/sentiment/twitter.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+import tweepy
+from hisa import AppConfig
+from hisa._util import _check_environment_variable_set
+
+class Twitter(object):
+    def __init__(self):
+        api_key                  = AppConfig.ENVIRONMENT_VARIABLE['twitter_api_key']
+        api_secret               = AppConfig.ENVIRONMENT_VARIABLE['twitter_api_secret']
+        access_token             = AppConfig.ENVIRONMENT_VARIABLE['twitter_access_token']
+        access_token_secret      = AppConfig.ENVIRONMENT_VARIABLE['twitter_access_token_secret']
+
+        _check_environment_variable_set(api_key, raise_err = True)
+        _check_environment_variable_set(api_secret, raise_err = True)
+        _check_environment_variable_set(access_token, raise_err = True)
+        _check_environment_variable_set(access_token_secret, raise_err = True)
+
+        self.api_key             = api_key
+        self.api_secret          = api_secret
+        self.access_token        = access_token
+        self.access_token_secret = access_token_secret
+
+        self.auth_handler        = tweepy.OAuthHandler(self.api_key, self.api_secret)
+        self.auth_handler.set_access_token(self.access_token, self.access_token_secret)
+
+        self.api                 = tweepy.API(self.auth_handler)
