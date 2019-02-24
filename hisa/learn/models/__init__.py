696e7ae76532dcce2a65a562f0e690998a14410a
Sahil Modi <smodi9@illinois.edu>
2019-02-24 09:57:50 -0600

    Update __init__.py

--- hisa/learn/models/__init__.py
+++ hisa/learn/models/__init__.py
@@ -3,5 +3,5 @@ from __future__ import absolute_import
 from hisa.learn import sentiment
 from hisa.learn.models.linearregression import LinearModel
 from hisa.learn.models.mlpregressor import MLPModel
-from hisa.learn.models.ridgregression import RidgeModel
+from hisa.learn.models.ridgeregression import RidgeModel
 from hisa.learn.models.preprocess import PreprocessData
