17826ea9189add3ec6a7704844b239dbb978acde
Sahil Modi <smodi9@illinois.edu>
2019-02-24 09:40:57 -0600

    dependecy fix

--- hisa/__init__.py
+++ hisa/__init__.py
@@ -3,5 +3,6 @@ from __future__ import absolute_import
 from hisa.capsule import Share
 from hisa.config import AppConfig
 from hisa.learn import sentiment
+from hisa._util import _util
 
 __version__ = AppConfig.VERSION
